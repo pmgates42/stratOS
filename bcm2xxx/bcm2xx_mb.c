ae7d3678fd7f272a71e3fa9c3f05a61b8cd30f39
Paul Gates <115600972+pmgates42@users.noreply.github.com>
2024-05-11 11:38:46 -0700

    WIP fix power issue

--- bcm2xxx/bcm2xx_mb.c
+++ bcm2xxx/bcm2xx_mb.c
@@ -11,6 +11,8 @@
 #include "bcm2xxx_mb.h"
 #include "peripherals/base.h"
 #include "printf.h"
+#include "utils.h"
+#include "../../drivers/usb/dwc2/dwc2.h"
 
 #define MB_FULL   0x80000000
 #define MB_EMPTY  0x40000000
@@ -21,16 +23,16 @@ enum
     POWER_CHNL = 0,
 };
 
-typedef struct
-{
-    reg32_t read_byt;
-    reg32_t __pad_0[5];
-    reg32_t status_byt;
-    reg32_t __pad_1[1];
-    reg32_t write_byt;
-} mb_reg_type;
+// typedef struct
+// {
+//     reg32_t read_byt;
+//     reg32_t __pad_0[5];
+//     reg32_t status_byt;
+//     reg32_t __pad_1[1];
+//     reg32_t write_byt;
+// } mb_reg_type;
 
-#define REG_MB ((volatile mb_reg_type *)(PBASE + 0xB880))
+#define REG_MB ((volatile uint32_t *)(PBASE + 0xB880))
 
 static uint32_t current_mb_val;
 
@@ -42,7 +44,7 @@ static uint32_t current_mb_val;
  */
 void bcm2xxx_mb_init(void)
 {
-current_mb_val = REG_MB->write_byt;
+current_mb_val = 0;
 
 }
 
@@ -58,16 +60,29 @@ void bcm2xxx_power_on(bcm2xxx_power_on_t type)
 {
     //TODO
     /* block for now. put this in a task */
-    while(REG_MB->status_byt & MB_FULL)
+    while(REG_MB[6] & MB_FULL)
         ;
 
-    //TODO abstract into helper function to set any channel
-    if( !(current_mb_val & type) )
+    current_mb_val |= type;
+    REG_MB[8] = ( current_mb_val & ~( 0x0F ) ) | POWER_CHNL;
+
+    delay(600);
+
+    #define REG_VBUS *( ( volatile uint32_t * )HPRT0 )
+    uint32_t hprt0 = REG_VBUS;
+    hprt0 &= ~(HPRT0_ENA | HPRT0_CONNDET | HPRT0_ENACHG | HPRT0_OVRCURRCHG);
+    if( !(hprt0 & HPRT0_PWR) )
     {
-        current_mb_val |= type;
-        REG_MB->write_byt = ( current_mb_val & ~( 0x0F ) ) | POWER_CHNL;
+        printf("vbus NOT on. Turning on vbus\n");
+        hprt0 |= HPRT0_PWR;
+        REG_VBUS = hprt0;
+
+        printf("vbus on");
+    }
+    else
+    {
+        printf("vbus on");
     }
 
-    printf("Successful power on of component %d", type);
+    // printf("Successful power on of component %d", type);
 }
-
